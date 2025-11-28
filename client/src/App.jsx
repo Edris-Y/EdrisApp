d9cd1bcb3abf5fecf36db7f090c8833ed9b41548
Edris <20222179@etud.univ-evry.fr>
2025-11-28 10:00:22 +0100

    Maj Site 28/11 p3

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,9 +5,9 @@ import About from "./pages/About";
 import Projects from "./pages/Projects";
 import Skills from "./pages/Skills";
 import Contact from "./pages/Contact";
+import Experience from "./pages/Experience";
 
-// On enlève le basename ou on le met à vide
-const basename = "/"; 
+const basename = "/EdrisApp";
 
 function App() {
   return (
@@ -18,6 +18,7 @@ function App() {
           <Route path="projects" element={<Projects />} />
           <Route path="skills" element={<Skills />} />
           <Route path="contact" element={<Contact />} />
+          <Route path="experience" element={<Experience />} />
           <Route path="about" element={<About />} />
         </Route>
       </Routes>
