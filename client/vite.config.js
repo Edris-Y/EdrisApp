df179e7e9af2f3a9bea12f43d56f225d5d761ff6
Edris <20222179@etud.univ-evry.fr>
2025-11-28 10:00:22 +0100

    Maj Site 28/11 p3

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // <-- nom EXACT du repo GitHub
+  base: '/EdrisApp/', // <-- nom EXACT du repo GitHub
 })
